6f96bfc9481e99117164c529cf074d0a1ea0872c
Gonzalo Alonso <gonkpo@gmail.com>
2018-10-26 21:24:19 -0300

    private

--- src/Controller/AfipController.php
+++ src/Controller/AfipController.php
@@ -9,23 +9,25 @@ use Afip;
  */
 class AfipController
 {
+    private $project_dir;
     private $afip_parameters;
     private $afip;
 
     public function __construct($afip_parameters, $project_dir)
     {
-        $this->parseParameters($afip_parameters, $project_dir);
+        $this->project_dir = $project_dir;
+        $this->parseParameters($afip_parameters);
 
         if ($this->afip_parameters) {
             $this->afip = new Afip($this->afip_parameters);
         }
     }
 
-    private function parseParameters($afip_parameters, $project_dir)
+    private function parseParameters($afip_parameters)
     {
         if ($afip_parameters['CUIT'] > 0) {
-            $afip_parameters['res_folder'] = $project_dir."/".$afip_parameters['res_folder'];
-            $afip_parameters['ta_folder'] = $project_dir."/".$afip_parameters['ta_folder'];
+            $afip_parameters['res_folder'] = $this->project_dir."/".$afip_parameters['res_folder'];
+            $afip_parameters['ta_folder'] = $this->project_dir."/".$afip_parameters['ta_folder'];
         } else {
             $afip_parameters = null;
         }
