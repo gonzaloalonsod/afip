ae1c43b76de1b49eafb29524357a8370cb26064c
Gonzalo Alonso <gonzaloalonsod@gmail.com>
2021-03-10 17:23:49 -0300

    Gonzaloalonsod

--- src/DependencyInjection/AfipExtension.php
+++ src/DependencyInjection/AfipExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Gonzakpo\AfipBundle\DependencyInjection;
+namespace Gonzaloalonsod\AfipBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
